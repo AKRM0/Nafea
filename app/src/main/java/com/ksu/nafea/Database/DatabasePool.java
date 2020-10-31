2b95fa33452faf6995be6f3e5da86d016e0397cf
Khalid <khalid.m.alnahed@gmail.com>
2020-10-31 10:56:18 +0300

    userpool&datapool

--- app/src/main/java/com/ksu/nafea/Database/DatabasePool.java
+++ app/src/main/java/com/ksu/nafea/Database/DatabasePool.java
@@ -1,6 +1,22 @@
 package com.ksu.nafea.Database;
 
-public class DatabasePool
-{
+import java.util.ArrayList;
 
+public class DatabasePool
+
+{
+    public boolean update(String new_value,String tableName,String Condition,String pos){
+        String command="UPDATE"+" "+tableName+" "+"SET"+" "+pos+" "+"="+" "+new_value+" "+"WHERE"+" "+Condition;
+
+}
+
+    public Object retrieve(String attribute,String tableName,String Condition) {
+        ArrayList<Object> Retrieved = new ArrayList();
+        String command = "SELECT " + " " + attribute + " " + "FROM" + " " + tableName + " " + "WHERE" + " " + Condition;
+
+    }
+    public boolean insert(String TableName,String att){
+        String command="INSERT INTO"+" "+ TableName+" " +att;
+        return true;
+    }
 }

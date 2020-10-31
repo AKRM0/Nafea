209f8153dd2b0f55f70e42c1c72b5ce492653de4
Abdulkareem <raiden.wg1@gmail.com>
2020-10-31 12:52:15 +0300

    Fix Init_Pools

--- app/src/main/java/com/ksu/nafea/Database/DatabasePool.java
+++ app/src/main/java/com/ksu/nafea/Database/DatabasePool.java
@@ -1,6 +1,27 @@
 package com.ksu.nafea.Database;
 
-public class DatabasePool
-{
+import java.util.ArrayList;
 
+public class DatabasePool
+
+{
+    public boolean update(String tableName, String Condition){
+        String command="UPDATE"+" "+tableName+" "+"SET"+" "+Condition;
+
+        // To-Do execute SQL command.
+        return false;
+    }
+
+    public Object retrieve(String attribute,String tableName,String Condition) {
+        ArrayList<Object> Retrieved = new ArrayList();
+        String command = "SELECT " + " " + attribute + " " + "FROM" + " " + tableName + " " + "WHERE" + " " + Condition;
+
+        // To-Do execute SQL command.
+        return null;
+    }
+    public boolean insert(String TableName,String att){
+        String command="INSERT INTO"+" "+ TableName+" " +att;
+        // To-Do execute SQL command.
+        return true;
+    }
 }

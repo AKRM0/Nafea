33bb4b5195eff48f9d006259c22c924838ae9e54
Abdulkareem <raiden.wg1@gmail.com>
2020-11-07 20:51:58 +0300

    Establish Database Connection

--- app/src/main/java/com/ksu/nafea/data/UserPool.java
+++ app/src/main/java/com/ksu/nafea/data/UserPool.java
@@ -1,17 +1,57 @@
 package com.ksu.nafea.data;
 
+import com.ksu.nafea.logic.User;
+import com.ksu.nafea.utilities.DatabaseException;
+
 public class UserPool extends DatabasePool
 {
-    public boolean insert(String s_email, String password, String firstName, String lastName)
+    public boolean insertStudent(String s_email, String password, String firstName, String lastName)  throws DatabaseException
     {
         String ins = "(" + s_email + "," + password + "," + firstName + "," + lastName + ")";
         return insert("Student", ins);
     }
-
-    public boolean update(String new_value, String pos, String s_email)
+    public boolean insertadmin(String a_email, String password, String firstName, String lastName) throws DatabaseException
     {
-        String condition = "SET" + " " + pos + " " + "=" + " " + new_value + " " + "WHERE s_email = " + s_email;
-        return update("Student", condition);
+        String ins = "(" + a_email + "," + password + "," + firstName + "," + lastName + ")";
+        return insert("Admin", ins);
+    }
 
+    public boolean updateStudent(String new_value, String position, String s_email) throws DatabaseException
+    {
+        String condition = "SET" + " " + position + " " + "=" + " " + new_value + " " + "WHERE s_email = " + s_email;
+        return Update( "Student", condition);
+    }
+    public boolean updateAdmin(String new_value, String position, String a_email) throws DatabaseException
+    {
+        String condition = "SET" + " " + position + " " + "=" + " " + new_value + " " + "WHERE e_email = " + a_email;
+        return Update( "Admin", condition);
+    }
+
+    public boolean deleteStudent(String condition) throws DatabaseException
+    {
+        String cond = "WHERE"+ ""+condition;
+        return delete("Student",cond);
+    }
+    public boolean deleteAdmin(String condition) throws DatabaseException
+    {
+        String cond = "WHERE"+ ""+condition;
+        return delete("Admin",cond);
+    }
+
+    public User retrieveStudent(String condition, String attribute) {
+        String command =  " " + "WHERE" + " " + condition;
+        //To-Do retrieve result Student as object,
+        //retrieve(attribute,"Admin",command);
+        User st = new User("","");
+        //assign attributes to st object
+        return st;
+    }
+    public User retrieveAdmin(String condition,String attribute){
+        String command =  " " + "WHERE" + " " + condition;
+        //To-Do retrieve result admin as object,
+        // retrieve(attribute,"Admin",command);
+        User ad = new User("","");
+        //assign attributes to ad object
+        return ad;
     }
 }
---
+++ app/src/main/java/com/ksu/nafea/utilities/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.ksu.nafea.utilities;
+
+public class DatabaseException extends Exception
+{
+    public DatabaseException(String msg)
+    {
+        super(msg);
+    }
+}
